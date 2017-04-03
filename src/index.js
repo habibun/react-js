783ea020fd4085732f2535b5b0fe101e06769ba1
Habibun Noby <habibun.noby@gmail.com>
2017-04-03 22:58:50 +0600

    react router version 4

--- src/index.js
+++ src/index.js
@@ -2,160 +2,47 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Todo from './Todo';
 import './index.css';
-import {Router, Route, browserHistory, IndexRoute, Link, Redirect} from 'react-router';
-import { style } from 'typestyle';
-import $ from "jquery";
+import {
+	BrowserRouter as Router, 
+	Route,
+	Link,
+	NavLink,
+} from 'react-router-dom';
 
-const test = style({
-	backgroundColor: 'yellow',
-	$nest: {
-		"& > h2": {
-			backgroundColor: 'red',
-		}
-	}
-});
 
-class Fetch extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			content: []
-		}
-	}
+const Home = () => (
+	<h1>Home</h1>
+)
 
-	componentDidMount() {
-		$.ajax({
-			url: this.props.url,
-			success: (data) => {
-				this.setState({
-					content: data
-				})
-			},
-			error: (error) => {
-				console.log("error", error)
-			}
-		})
-	}
+const About = () => (
+	<h1>About</h1>
+)
 
-	render() {
-		return(
-			<section>
-				{ this.props.children(this.state.content) }
-			</section>
-		)
-	}
-}
+
+const Links = () => (
+	<ul>
+		<li><NavLink exact activeClassName="active" to="/">Home</NavLink></li>
+		<li><NavLink activeClassName="active" to="/about">About</NavLink></li>
+	</ul>
+)
+
 
 class App extends React.Component {
 	render() {
-		return(
-			<section>
-				<Fetch url="https://jsonplaceholder.typicode.com/posts" >
-				{
-					(data) => {
-						return data.map((value, index) => {
-							return <li key={index}>{value.title}</li>
-						})
-					}
-				}
-				</Fetch>
-
-			</section>
-			)
+		return (
+			<Router>
+				<section>
+					<Links/>
+					<Route exact path="/" component={Home} />
+					<Route path="/about" component={About} />
+				</section>
+			</Router>
+		);
 	}
 }
-
-const AboutPage = (props) => (
-	<section>
-		<h2>This is About page</h2>
-		<div>{props.childen}</div>
-		<Link activeClassName="active" to="/about/nestedone">Nested one</Link>
-		{' '}
-		<Link activeClassName="active" to="/about/nestedtwo">Nested two</Link>
-	</section>
-)
-
-const HomePage = (props) => (
-	<section>
-		<div className={test}>
-			<h2 className={test}>This is Home page</h2>
-			<p>this is a test</p>
-		</div>
-
-		<Link to="/about">About</Link>
-		{' '}
-		{' '}
-		<Link to="/querystring">Query string</Link>
-		{props.children}
-
-	</section>
-)
-
-const NestedOne = () => (
-	<section>
-		<h2>Nested page 1</h2>
-	</section>
-)
-
-class Counter extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			counter: 0
-		}
-	}
-
-	increaseCounter(){
-		/*this.setState({
-			counter: this.state.counter + 1
-		}, () => {
-			console.log(this.state.counter)
-		})*/
-
-		// console.log(this.state.counter)
-
-		this.setState((prevState) => {
-			return {
-				counter: prevState.counter + 1
-			}
-		}, () => {
-		console.log(this.state.counter)
-			
-		})
-		// console.log(this.state.counter)
-
-	}
-
-	render() {
-		return(
-			<section>
-			{this.state.counter}
-			<button onClick={this.increaseCounter.bind(this)}>Add </button>
-			</section>
-		)
-	}
-}
-
-
-const NestedTwo = () => (
-	<section>
-		<h2>Nested page 2</h2>
-	</section>
-)
-
-
-const IndexRoutePage = () => (
-	<h2>IndexRoutePage Page</h2>
-)
-
-const QueryString = (props) => (
-	<section>
-		<h3>Query string test </h3>
-		<p>my param: {props.routeParams.id}</p>
-	</section>
-)
 
 ReactDOM.render(
-  <Counter/>,
+  <App/>,
   document.getElementById('root')
 );
+
